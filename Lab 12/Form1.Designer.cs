4b2e089c80a416ecfa4a1f413625f7a5232b3993
YahyaAbdulMajeed <yahya.zf2@gmail.com>
2023-06-13 17:48:57 +0700

    Add project files.

---
+++ Lab 12/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Lab_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(61, 67);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(155, 45);
+            this.btnStartStop.TabIndex = 1;
+            this.btnStartStop.Text = "Start/Stop";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(447, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 198);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStartStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Lab 12/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_12
+{
+    public partial class Form1 : Form
+    {
+        private double[,] transitionRateMatrix = {
+            { -0.4, 0.3, 0.1},
+            { 0.4, -0.8, 0.4 },
+            { 0.1, 0.4, -0.5 }
+        };
+        private double[,] CDFS = new double[3,3];
+        private Dictionary<int, string> states = new Dictionary<int, string>()
+        {
+            {0,"C:\\Users\\yahya\\source\\repos\\Lab 12 and 13\\Lab 12\\Images\\cloudy.png" },
+            {1,"C:\\Users\\yahya\\source\\repos\\Lab 12 and 13\\Lab 12\\Images\\raining.png" },
+            {2,"C:\\Users\\yahya\\source\\repos\\Lab 12 and 13\\Lab 12\\Images\\sun.png" }
+        };
+        private int currentState = 0;
+        private int T = 10000;
+        private int N = 6;
+        private int count = 0;
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            CalculateCDFs();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled) timer1.Stop();
+            else
+            {
+                pictureBox1.SizeMode =PictureBoxSizeMode.StretchImage;
+                timer1.Interval = CalculateHoldingTimeInMilliSeconds(transitionRateMatrix[currentState,currentState]*-1);
+                pictureBox1.ImageLocation = states[currentState];
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentState = GetNextState();
+            pictureBox1.ImageLocation = states[currentState];
+            timer1.Interval = CalculateHoldingTimeInMilliSeconds(transitionRateMatrix[currentState,currentState]*-1);
+
+        }
+        private int GetNextState()
+        {
+            var U = random.NextDouble();
+            for(int i = 0; i < 3; i++)
+            {
+                if (U - CDFS[currentState, i] <= 0)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+        private void CalculateCDFs()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                double sum = 0;
+                for(int j = 0; j < 3; j++)
+                {
+                    if (i == j)
+                        sum += 0;
+                    else
+                        sum += (transitionRateMatrix[i, j] / -transitionRateMatrix[i,i]);
+                    CDFS[i,j] = sum;
+                }
+            }
+        }
+        private int CalculateHoldingTimeInMilliSeconds(double rate)
+        {
+            return (int)((-(Math.Log(random.NextDouble())) / rate)*1000);
+        }
+
+    }
+}
