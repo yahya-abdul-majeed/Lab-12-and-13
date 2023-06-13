39711d2f0d8f5f58840db7dcf9573c30b9753488
YahyaAbdulMajeed <yahya.zf2@gmail.com>
2023-06-14 00:07:29 +0700

    Added lab 13

---
+++ Lab 13/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Lab_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtcBox = new System.Windows.Forms.NumericUpDown();
+            this.EthBox = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtcBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EthBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(264, 103);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Bitcoin";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Ethereum";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(482, 314);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(594, 30);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(119, 34);
+            this.btnStartStop.TabIndex = 1;
+            this.btnStartStop.Text = "Start/Stop";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bitcoin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ethereum";
+            // 
+            // BtcBox
+            // 
+            this.BtcBox.DecimalPlaces = 2;
+            this.BtcBox.Location = new System.Drawing.Point(105, 103);
+            this.BtcBox.Name = "BtcBox";
+            this.BtcBox.Size = new System.Drawing.Size(101, 22);
+            this.BtcBox.TabIndex = 4;
+            this.BtcBox.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // EthBox
+            // 
+            this.EthBox.DecimalPlaces = 2;
+            this.EthBox.Location = new System.Drawing.Point(105, 142);
+            this.EthBox.Name = "EthBox";
+            this.EthBox.Size = new System.Drawing.Size(101, 22);
+            this.EthBox.TabIndex = 5;
+            this.EthBox.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EthBox);
+            this.Controls.Add(this.BtcBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtcBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EthBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown BtcBox;
+        private System.Windows.Forms.NumericUpDown EthBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab 13/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_13
+{
+    public partial class Form1 : Form
+    {
+        double btcInitial;
+        double ethInitial;
+        double dt = 1.0 / 252.0; // time increment of one trading day
+        double t = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();            }
+            else
+            {
+                btcInitial = (double)BtcBox.Value;
+                ethInitial = (double)EthBox.Value;
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                t = 0;
+                timer1.Interval = 100;
+                timer1.Start();
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var btcPrice = GeometricBrownianMotion.CalculateS1(btcInitial, 0.09, 2, dt);
+            var ethPrice = GeometricBrownianMotion.CalculateS1(ethInitial, 0.02, -0.9, dt);
+            t += dt;
+            chart1.Series[0].Points.AddXY(t,btcPrice);
+            chart1.Series[1].Points.AddXY(t,ethPrice);
+
+        }
+    }
+    public class GeometricBrownianMotion
+    {
+        public static double CalculateS1(double S0, double mu, double sigma, double dt)
+        {
+            Random random = new Random();
+            double deltaW = Math.Sqrt(dt) * random.NextGaussian();
+            double drift = (mu - 0.5 * sigma * sigma) * dt;
+            double diffusion = sigma * deltaW;
+            double S1 = S0 * Math.Exp(drift + diffusion);
+            return S1;
+        }
+    }
+
+    public static class RandomExtensions
+    {
+        public static double NextGaussian(this Random random)
+        {
+            double u1 = 1.0 - random.NextDouble();
+            double u2 = 1.0 - random.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+            return randStdNormal;
+        }
+    }
+}
